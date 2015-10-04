bae8627d4a23623dfa8dbb136b72fe42bc3a884e
Wilfried Meindl <wilfried.meindl@gmail.com>
2015-10-04 17:36:04 +0200

    initial import

---
+++ emptytrash-posix.sh
@@ -0,0 +1,38 @@
+  #!/bin/sh
+
+# emptytrash
+# Regelmaessig von cron ausfuehren lassen.
+# Leert die Trash-Verzeichnisse, wenn sie die Maximalgroesse uebersteigen.
+
+trashlog=/.trash/trash.log	# entfernte Dateien loggen
+datum=$(date +'%y/%m/%d %H:%M')
+tmpfile=/tmp/emptytrash.tmp
+
+# Maximalgroessen in Bytes
+maxsizes=" /.trash       50000000 
+	   /usr/.trash   50000000 
+	   /home/.trash 100000000 
+	   /var/.trash   50000000 "
+
+echo "$maxsizes" |
+
+# Schleife um Trash-Verzeichnisse  
+while read trash maxsize
+do
+
+  realsize=$(du -sx $trash | cut -w -f1)
+  realsize=$((realsize*1024))			# jetzt in Bytes
+
+  find "$trash" -print0 | xargs -0 ls -ldtr > "$tmpfile" # zeitlich sortiert
+  while  [ $realsize -gt $maxsize ] && read x x x x fsize x x x file
+  do
+    [ X"$file" == X"$trashlog" ] && continue
+    : $((realsize-=$fsize))
+    if [ -d "$file" ]
+    then rmdir "$file" 2> /dev/null
+    else rm "$file"    2> /dev/null
+    fi
+    [ "$?" = "0" ] && echo "$file" "entfernt $datum" >> $trashlog
+  done <"$tmpfile"
+
+done
---
+++ trash-posix.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+# trash
+# Features:
+# Alias fuer 'rm' oder 'del'.
+# Verschiebt Dateien in den Papierkorb.
+# Innerhalb des Papierkorbs wird richtig geloescht.
+# Mit Versionskontrolle
+
+# $trash und $trashlog muessen schreibbar fuer alle sein.
+
+trashdir=/.trash
+trashlog=/.trash/trash.log	# Logging endgueltig entfernter Dateien
+pwddir=$PWD
+datum=`date +'%y/%m/%d %H:%M'`
+
+# Optionen
+# --------
+if [ X"$1" = X"-r" ] ; then
+  recursive="yes"
+  shift
+fi
+
+
+# Hauptteil
+# ---------
+
+for file in "$@"
+do
+  # Ziel-Verzeichnis bestimmen
+  # --------------------------
+  reldir=$(dirname "$file")
+  name=$(basename "$file")
+  case "$reldir" in
+    /*) directory="$reldir" ;;
+     .) directory="$pwddir" ;;
+     *) directory=$pwddir/$reldir
+  esac
+
+  # Zur Sicherheit: Verzeichisse sind nur mittels -r loeschbar
+  # ----------------------------------------------------------
+  if [ -d "$file" ] && [ "$recursive" != "yes" ] ; then
+    echo "$file ist ein Verzeichnis. Verwenden Sie rm -r."
+    continue
+  fi
+
+  # Verschieben oder Loeschen
+  # -------------------------
+  absdir=$(cd "$directory" 2>/dev/null; pwd)
+  if [ X"$absdir" = X${absdir#$trashdir} ]   # are we in the trash directory
+  then
+    # 2 Versionen erhalten
+    if [ -e "$trashdir/$directory/$name" ] ; then
+      /bin/rm -r "$trashdir/$directory/$name.1" 2>/dev/null
+      mv "$trashdir/$directory/$name" "$trashdir/$directory/$name.1"
+    fi
+    # In den Trash verschieben
+    # ------------------------
+    mkdir -p "$trashdir/$directory" 2>/dev/null
+    mv "$file" "$trashdir/$directory"
+  else
+    # Richtiges Loeschen im Trash
+    # ---------------------------
+    if [ -d "$file" ]
+    then /bin/rm -r "$file"
+    else /bin/rm "$file"
+    fi
+      echo "$file entfernt $datum" >> "$trashlog"
+  fi
+done
